4500221a8640f8f5d8f9f729b7af341de23293e4
Danilo Borim <daniloborimfelix20@gmail.com>
2023-03-02 11:54:42 -0300

    .

--- workus.php
+++ workus.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="img/iconhead.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link rel="stylesheet" href="Style/workus.css">
-    <link rel="stylesheet" href="Style/footer.css">
+    <link rel="stylesheet" href="Style/rodape.css">
     <link rel="stylesheet" href="Style/menu.css">
     <title>Document</title>
 </head>
