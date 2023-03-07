2fc25d4538cc7aa1a07a634f3b1368513c4b3647
Danilo Borim <daniloborimfelix20@gmail.com>
2023-03-07 08:55:59 -0300

    .

--- workus.php
+++ workus.php
@@ -16,18 +16,22 @@
         <main class="principal">
             <div class="work_container">
                 <div class="contact_box">
-                    <div class="input">
-                        <h2>Trabalhe Conosco</h2>
-                        <input type="text" class="field" placeholder="Seu Nome">
-                        <input type="email" class="field" placeholder="Seu Email">
-                        <input type="text" class="field" placeholder="Seu Telefone">
-                        <textarea class="field" placeholder="Mensagem"></textarea>
-                        <button>Enviar</button>
-                    </div>
+                    <form class="contact_form" action="#">      
+                        <div class="input">        
+                            <h2>Trabalhe Conosco</h2>
+                            <input type="text" name="name" class="field name" placeholder="Seu Nome">
+                            <input type="email" name="email" class="field email" placeholder="Seu Email">
+                            <input type="text" class="field tel" placeholder="Seu Telefone">
+                            <textarea name="message" class="field msg" placeholder="Digite sua mensagem"></textarea>
+                            <button type="submit">Enviar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </main>
-
+    <script src="https://smtpjs.com/v3/smtp.js"></script>
+    <script src="Scripts/email.js"></script>
     <?php include("footer.php");?>
     </body>
+
 </html>
