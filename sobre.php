72211143c8a138c53ddc9373277425bb48c2918c
Danilo Borim <daniloborimfelix20@gmail.com>
2023-03-13 17:40:13 -0300

    Página Jornal feita

--- sobre.php
+++ sobre.php
@@ -35,7 +35,7 @@
                 <div class="enterprise">
                     <div class="alter"> 
                         <h3>A Empresa</h3>
-                        <p>Com um total de 54 funcionarios atualmente, a empresa No Caminho do Êxito se consolida no mercado com a ideia de diminuir burocracias dentro da distribuição de produtos.</p>
+                        <p>Com um total de 84 funcionarios atualmente, a empresa No Caminho do Êxito se consolida no mercado com a ideia de diminuir burocracias dentro da distribuição de produtos.</p>
                         <p>Temos a filosofia de um ambiente de trabalho descontraido e livre para que o funcionario possa se sentir a vontade e tenha Êxito em suas missões diárias acompanhado de uma constante evolução.</p>          
                     </div>
                     <img src="img/empresa1.jpeg" alt="empresa">  
@@ -44,9 +44,11 @@
                 <div class="ceo">
                     <div class="alter">
                         <h3>O CEO</h3>
-                        <p>Responsavel pela criação e desenvolvimento da empresa. Sempre com determinação e constancia em seus objetivos. Atualmente mantém reuniões diárias que também são palestras para os funcionarios.
-                            Grande defensor da disciplica e dedicação, Diogo acredita que um bom ambiente de trabalho junto de um incentivo para os funcionários seja a chave para o sucesso.
-                        </p>
+                        <p>“Empresário… Diretor Comercial… Ninguém pergunta o seu cargo, mas sim sua profissão e a minha profissão eu escrevo com letra grande, com o maior orgulho, VENDEDOR”
+
+                        <p>- Diogo Tomas Figueiredo</p>
+
+                        <p>Responsável pela criação e desenvolvimento da empresa. Atualmente mantém reuniões diárias que também são palestras para a equipe. Grande defensor da disciplina, dedicação, determinação e constância em seus objetivos, Diogo acredita que um bom ambiente de trabalho junto de um incentivo para os funcionários seja a chave para o êxito.</p>
                     </div>
                     <img src="img/equipe/diogo.png">
                 </div>
@@ -120,12 +122,6 @@
 
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
-                        <span>Vitor</span>
-                        <p>Supervisor de RH</p>
-                    </div>
-
-                    <div class="team_card">
-                        <img src="img/hebert.png" alt="pessoa">
                         <span>Gabriel Fonseca</span>
                         <p>Supervisor de Qualidade</p>
                     </div>
@@ -133,74 +129,74 @@
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Gabriel Barros</span>
-                        <p id="partners">Supervisor de Qualidade</p>
+                        <p>Supervisor de Qualidade</p>
                     </div>
 
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Carlos </span>
-                        <p id="partners">Supervisor de Qualidade</p>
+                        <p>Supervisor de Qualidade</p>
                     </div>
 
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Marcelo</span>
-                        <p id="partners">Gerente de Vendas</p>
+                        <p>Gerente de Vendas</p>
                     </div>
 
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Sabrina</span>
-                        <p id="partners">Gerente de Vendas</p>
+                        <p>Gerente de Vendas</p>
                     </div>
                     
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Gustavo Cortez</span>
-                        <p id="partners">Supervisor de Vendas</p>
+                        <p>Supervisor de Vendas</p>
                     </div>
                     
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Pedro Sofilio</span>
-                        <p id="partners">Supervisor de Vendas</p>
+                        <p>Supervisor de Vendas</p>
                     </div>
 
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Victória</span>
-                        <p id="partners">Supervisora de Vendas</p>
+                        <p>Supervisora de Vendas</p>
                     </div>
 
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Thais</span>
-                        <p id="partners">Supervisora de Vendas</p>
+                        <p>Supervisora de Vendas</p>
                     </div>
 
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Daniele</span>
-                        <p id="partners">Supervisora de Vendas</p>
+                        <p>Supervisora de Vendas</p>
                     </div>
 
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Pi</span>
-                        <p id="partners">Supervisora de Vendas</p>
+                        <p>Supervisora de Vendas</p>
                     </div>
 
                     <div class="team_card">
                         <img src="img/hebert.png" alt="pessoa">
                         <span>Gabriela</span>
-                        <p id="partners">Gestora de Vendas</p>
+                        <p>Gestora de Vendas</p>
                     </div>
 
                 </div>  
                      <!--ALTERAR IMAGENS APÓS FAZER O BACKEND-->
                     <div class="brands">
                         
-                        <h3>Marcas Parceiras</h3>
+                        <h3 id="partners">Marcas Parceiras</h3>
 
                         <img class="mudam1" src="img/marcas/marcasdeitadas.png">
                         <img class="mudam2" src="img/marcas/marcasempe.png">
--- workus.php
+++ workus.php
@@ -9,36 +9,21 @@
     <link rel="stylesheet" href="Style/workus.css">
     <link rel="stylesheet" href="Style/rodape.css">
     <link rel="stylesheet" href="Style/menu.css">
-    <title>Document</title>
+    <title>Trabalhe Conosco</title>
 </head>
+
     <body>
     <?php include("menu.php");?>
 
-
         <main class="principal">
-            <div class="work_container"> 
-                <h1>Nos contate!</h1>
-                <p>Se sinta livre para nos mandar uma mensagen!</p>
-                
-                <form class="contact_form" action="">    
-                    <label for="nome">Seu nome: </label>
-                    <input class="nome" type="text" name="nome">
-                    <label for="email">Seu email: </label>
-                    <input class="email" type="email" name="email">
-                    <label for="assunto">Assunto: </label>
-                    <input class="assunto" type="text" name="assunto">
-                    <label for="message">Mensagem: </label>
-                    <textarea class="message" name="message" cols="30" rows="10"></textarea>
-                    <input type="submit" value="Enviar">
-                </form>                    
-                    
-            </div>
+
+
+
         </main>
-
-
-        <script src="https://smtpjs.com/v3/smtp.js"></script>
-        <script src="Scripts/mail.js"></script>
-    <?php include("footer.php");?>
+     
+        <?php include("footer.php");?>
+    
     </body>
 
+    <script src="Scripts/nav.js"></script>
 </html>
