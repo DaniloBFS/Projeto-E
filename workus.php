99536a49e5c2e2f2568527c9efa71d83d8e622ac
Danilo Borim <daniloborimfelix20@gmail.com>
2023-03-14 11:37:24 -0300

    Pronto para merge

--- workus.php
+++ workus.php
@@ -1,3 +1,32 @@
+<?php
+  if (isset($_POST["submit"])) {
+    $username = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+
+    $to = $email;
+    $subject = $message;
+
+    $message = "Name: {$username} Email: {$email} Phone: {$phone}  Message: " . $message;
+
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    // More headers
+    $headers .= 'From: nocaminhodoexitoti@gmail.com';
+
+    $mail = mail($to,$subject,$message,$headers);
+
+    if ($mail) {
+      echo "<script>alert('Mail Send.');</script>";
+    }else {
+      echo "<script>alert('Mail Not Send.');</script>";
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +38,9 @@
     <link rel="stylesheet" href="Style/workus.css">
     <link rel="stylesheet" href="Style/rodape.css">
     <link rel="stylesheet" href="Style/menu.css">
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"></script>
     <title>Trabalhe Conosco</title>
 </head>
 
@@ -17,13 +49,86 @@
 
         <main class="principal">
 
+        <div class="container">
+      <span class="big-circle"></span>
+      <img src="img/shape.png" class="square" alt="" />
+      <div class="form">
+        <div class="contact-info">
+          <h3 class="title">Fale Conosco!</h3>
+          <p class="text">
+            Sinta-se livre para nos mandar um email e venha trabalhar conosco!
+          </p>
 
+          <div class="info">
+            <div class="information">
+              <img src="img/contato/location.png" class="icon" alt="" />
+              <p>Rua Frei Caneca, 1246, Bairro Consolação - São Paulo</p>
+            </div>
+            <div class="information">
+              <img src="img/contato/email.png" class="icon" alt="" />
+              <p>nocaminhodoexitoti@gmail.com</p>
+            </div>
+            <div class="information">
+              <img src="img/contato/phone.png" class="icon" alt="" />
+              <p>11 948244374</p>
+            </div>
+          </div>
 
-        </main>
+          <div class="social-media">
+            <p>Se conecte com a gente :</p>
+            <div class="social-icons">
+              <a href="https://www.facebook.com/profile.php?id=100089795936303">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="https://www.tiktok.com/@nocaminhodoexito?lang=pt-BR">
+                <i class="fab fa-tiktok"></i>
+              </a>
+              <a href="https://www.instagram.com/exito_ltda/">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/company/%C3%AAxito-ltda/?viewAsMember=true">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="contact-form">
+          <span class="circle one"></span>
+          <span class="circle two"></span>
+
+          <form action="" method="post" autocomplete="off">
+            <h3 class="title">Nos mande uma mensagem</h3>
+            <div class="input-container">
+              <input type="text" name="name" class="input" />
+              <label for="">Usuário</label>
+              <span>Usuário</span>
+            </div>
+            <div class="input-container">
+              <input type="email" name="email" class="input" />
+              <label for="">Email</label>
+              <span>Email</span>
+            </div>
+            <div class="input-container">
+              <input type="tel" name="phone" class="input" />
+              <label for="">Telefone</label>
+              <span>Telefone</span>
+            </div>
+            <div class="input-container textarea">
+              <textarea name="message" class="input"></textarea>
+              <label for="">Mensagem</label>
+              <span>Mensagem</span>
+            </div>
+            <input type="submit" name="submit" value="Send" class="btn" />
+          </form>
+        </div>
+      </div>
+    </div>
+
+    </main>
      
-        <?php include("footer.php");?>
-    
-    </body>
 
+    </body>
+    <script src="Scripts/workus.js"></script>
     <script src="Scripts/nav.js"></script>
 </html>
